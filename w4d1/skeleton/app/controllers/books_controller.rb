e87fa47b2d30af5469a7e80e3ef24b9bda0718ab
Parker Fritch <pjfritch1@gmail.com>
2017-08-22 06:30:40 -0700

    controllers

---
+++ w4d1/skeleton/app/controllers/books_controller.rb
@@ -0,0 +1,26 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+
+    render :index
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    # your code here
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_url
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
+end
