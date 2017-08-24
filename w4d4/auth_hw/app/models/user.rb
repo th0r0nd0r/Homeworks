ac9efe3eb4891676fa3dada9830af62c0c9b340e
Parker Fritch <pjfritch1@gmail.com>
2017-08-24 00:56:48 -0700

    flesh out user

--- w4d4/auth_hw/app/models/user.rb
+++ w4d4/auth_hw/app/models/user.rb
@@ -6,10 +6,32 @@ class User < ApplicationRecord
 
 
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
   def is_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
   end
 end
