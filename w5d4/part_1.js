285c0a0729f9e04a6de6a7fe981652e3faab4341
Parker Fritch <pjfritch1@gmail.com>
2017-08-31 07:57:44 -0700

    more js

---
+++ w5d4/part_1.js
@@ -0,0 +1,11 @@
+function madLib(verb, adjective, noun) {
+  return `We shall ${verb} the ${adjective} ${noun}`
+}
+
+// madLib('squish', 'squishy', 'squanch')
+
+function isSubstring(searchString, subString) {
+  return searchString === subString
+}
+
+isSubstring("a", "a")
